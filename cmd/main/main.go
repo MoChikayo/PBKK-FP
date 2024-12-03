dda681b04281e7face93f41f2ff1ac2903047eaa
MoChikayo <127732169+MoChikayo@users.noreply.github.com>
2024-12-03 08:57:15 +0700

    Main, Utils

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/MoChikayo/PBBK-FP/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	log.Println("Server is running on http://localhost:9010")
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
---
+++ pkg/models/book.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"PBBK-FP/pkg/config"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	if err := db.Create(&b).Error; err != nil {
+		return nil
+	}
+	return b
+}
+
+func GetAllBooks() ([]Book, error) {
+	var books []Book
+	err := db.Find(&books).Error
+	return books, err
+}
+
+func GetBookById(id int64) (*Book, error) {
+	var book Book
+	err := db.First(&book, id).Error
+	return &book, err
+}
+
+func DeleteBook(id int64) error {
+	return db.Delete(&Book{}, id).Error
+}
